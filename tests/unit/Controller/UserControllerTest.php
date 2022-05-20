4e6d6ec2e04b2577f8ec70e03d77236c28fef02d
Vitor Mattos <vitor@php.rio>
2022-05-20 14:34:05 -0300

    Add PHPUnit and bootstrap

---
+++ tests/unit/Controller/UserControllerTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class UserControllerTest extends TestCase
+{
+    public function testTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
